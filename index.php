17d351c65989ee03cb2592e140b70bb9cf690c89
maniu <smanus056@gmail.com>
2019-11-14 21:35:05 -0600

    Primer Commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+<meta charset="UTF-8">
+<title>Login-magtimusPro</title>
+ <link rel="stylesheet" href="css/style.css">
+ <meta name="viewport" content="width=device-width, user-scalable=no,
+       initial-scale=1.0, maximun-scale=1.0">
+
+  </head>
+ 
+<body>
+<div class="container-all">
+   <div class="ctn-form"> 
+    <img src="imagenes/rap.png" alt=""class="logo">
+    <h1 class="title">Iniciar Sesion</h1>
+     
+
+    <form action="">
+       <label for="">Email</label>
+       <input type="text">
+         <label for="">Contraseña</label>
+         <input type="password">
+      <input type="submit" value="iniciar">
+    </form>
+
+
+    <span  class="text-footer">No eres cliente?<a href="#">Registrate</a></span>
+   
+</div>
+<div class="ctn-texto">
+    <div class="capa"></div>
+    <h1 class="title-description">RAPIFOODS todo a tu alcance.</h1>
+    <p class="text-description">Somos una empresa para falicitar tu consumo
+        tu pide lo quer quieras nosotros te lo llevamos hasta la puerta de tu casa o tu ubicacion
+        confia en RRAPIFOODS</p>
+</div> 
+</div>
+</body>
+
+</html>
